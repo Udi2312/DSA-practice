b19d80825d481e8175b0b5bb1e6fd76c24cdce1f
Udit Bansal <udit.ban2312@gmail.com>
2025-09-02 01:49:31 +0530

    commit

---
+++ pattern printing/pattern printing.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class abc{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int r = s.nextInt();
+        int c = s.nextInt();
+
+        // Question 1
+        // for(int i = 0; i<rows; i++){
+        //     for(int j =0; j<cols; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Question 2
+        // for(int i = 0; i<rows; i++){
+        //     for(int j =0; j<cols; j++){
+        //         if(i==0 || j==0 || i==rows-1 || j == cols-1){
+        //             System.out.print("*");
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Question 3
+        // for(int i = 0; i<r; i++){
+        //     for(int j =0; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+}
+}
