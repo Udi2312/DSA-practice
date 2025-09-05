adf3cc049da1c417c2e7b8c33747804031ab125b
Udit Bansal <udit.ban2312@gmail.com>
2025-09-05 21:08:38 +0530

    commit

--- pattern printing/pattern printing.java
+++ pattern printing/pattern printing.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 class abc{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int r = s.nextInt();
-        int c = s.nextInt();
+        System.out.println("Enter no. of rows: ");
+        // int r = s.nextInt();
+        // int c = s.nextInt();
 
         // Question 1
         // for(int i = 0; i<rows; i++){
@@ -35,5 +36,48 @@ class abc{
         //     }
         //     System.out.println();
         // }
+
+
+        // Question 4
+        //  for(int i = 0; i<r; i++){
+        //     for(int j =r-1; j>=i; j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Question 5
+        // int n = 2*r -1;
+        // for(int i = 0; i<r; i++){
+        //     // for(int j =0 ; j<n ; j++){
+        //         for(int k = 0; k<r-1-i; k++){
+        //             System.out.print(" ");
+        //         }
+        //         for(int l = 0; l<2*i+1; l++){
+        //             System.out.print("*");
+        //         }
+        //     // }
+        //     System.out.println();
+        // }
+
+
+        // Question 6
+        // for(int i = 0; i<7; i++){
+        //     int k = i+1;
+        //     for(int j = 0; j<7; j++){
+        //         if(k<=7){
+        //             System.out.print(k++ + " ");
+        //         }
+        //         else{
+        //             System.out.print(k++ - 7 + " ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // Question 7
+        
 }
 }
