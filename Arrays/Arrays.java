b9a479e8dcfb5e1068decf0942885d2b95aa0ac9
Udit Bansal <udit.ban2312@gmail.com>
2025-09-14 21:10:54 +0530

    commit

---
+++ Arrays/Arrays.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        // Count the number of occurances of a particular element x
+    //     int arr [] = {5,6,5,1,5,6};
+    //     int x = s.nextInt();
+    //     int count = 0;
+    //     for(int i =0; i<arr.length; i++){
+    //         if(arr[i] == x){
+    //             count++;
+    //         }
+    //     }
+    //     System.out.println(count);
+    // }
+}
