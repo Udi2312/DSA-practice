ff47c395c3cc4a202420d1bbebd498d2f5977522
Udit Bansal <udit.ban2312@gmail.com>
2025-09-15 15:43:18 +0530

    commit

--- Arrays/Arrays.java
+++ Arrays/Arrays.java
@@ -1,5 +1,46 @@
 import java.util.Scanner;
 public class Arrays {
+
+    static boolean checkSortedArray(int arr[]){
+        boolean flag = true;
+        for(int i =0; i<arr.length-1; i++){
+            if(arr[i] > arr[i+1]){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    static int lastOccurrence(int[] arr, int x){
+        int ans = -1;
+        for(int i = arr.length-1; i>=0; i--){
+            if(arr[i] == x){
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+
+    static void printArray(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+  static int[] takeArrayInput(){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter the size of array");
+    int n = s.nextInt();
+
+    System.out.println("Enter " + n +" elements: ");
+    int[] arr = new int[n];
+    for(int i =0; i<n; i++){
+         arr[i] = s.nextInt();
+    }
+    return arr;
+   }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         
@@ -13,5 +54,19 @@ public class Arrays {
     //         }
     //     }
     //     System.out.println(count);
+
+
+    // Find the last occurance of an element x in a given array
+//     int arr[] = takeArrayInput();
+//     System.out.println("Enter the number whose last occurance is to be found: ");
+//     int x = s.nextInt();
+//    int ans =  lastOccurrence(arr, x);
+//    System.out.print("last occurance of x is at index: "+ " "+ ans);
+
+
+    // Check if the given array is sorted or not
+    // int arr[] = takeArrayInput();
+    // System.out.println(checkSortedArray(arr));
+
     }
 }
