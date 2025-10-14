c2e0812707e3184be7df0927af54de8dee4f00c5
Udit Bansal <udit.ban2312@gmail.com>
2025-10-14 23:50:11 +0530

    commit

--- Arrays/TwoDArrays.java
+++ Arrays/TwoDArrays.java
@@ -1,5 +1,42 @@
 import java.util.Scanner;
 public class TwoDArrays {
+    static void transposeInPlace(int a[][] , int r, int c){
+        if(r!=c){
+            System.out.println("Wrong Input");
+            return;
+        }
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j<c; j++){
+                if(i<j){
+              int c1 = 0;
+              c1 = a[i][j];
+              a[i][j] = a[j][i];
+              a[j][i] = c1;
+                }
+            }
+        }
+        print2DArray(a, r, c);
+    }
+    static void swapin2Darray(int a[][] , int i1, int j1, int i2, int j2){
+          int c = 0;
+          c = a[i1][j1];
+          a[i1][j1] = a[i2][j2];
+          a[i2][j2] = c;
+        }
+        static void transposeofmatrix(int[][] a, int r , int c ){
+            System.out.println("Input Array");
+            print2DArray(a,r,c);
+            // int r = a.length;
+            // int c = a[0].length;
+            int[][] ans = new int[c][r];
+            for(int i = 0; i<c; i++){
+                for(int j = 0; j<r; j++){
+                    ans[i][j] = a[j][i];
+                }
+            }
+            System.out.println("Transpose Array");
+            print2DArray(ans, c, r);
+    }
     static void multiply2matrices(int[][] a, int[][] b){
         if(a[0].length != b.length){
             System.out.println("Wrong input: ");
@@ -14,7 +51,7 @@ public class TwoDArrays {
                 }
             }
         }
-        print2DArray(mul);
+        print2DArray(mul,a.length,b[0].length);
 
     }
     static void add2matrices(int a[][] , int b[][]){
@@ -28,7 +65,7 @@ public class TwoDArrays {
                 sum[i][j] = a[i][j] + b[i][j];
             }
         }
-        print2DArray(sum);
+        print2DArray(sum,a.length,a[0].length);
     }
     static int[][] take2DArrayInput(){
         Scanner s = new Scanner(System.in);
@@ -45,9 +82,9 @@ public class TwoDArrays {
         }
         return arr;
     }
-    static void print2DArray(int arr[][]){
-        for(int i = 0; i<arr.length; i++){
-            for(int j = 0; j<arr[0].length; j++){
+    static void print2DArray(int arr[][] , int r, int c){
+        for(int i = 0; i<r; i++){
+            for(int j = 0; j<c; j++){
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
@@ -59,10 +96,13 @@ public class TwoDArrays {
         // print2DArray(arr2);
         
         // Multiply 2 matrices
-        // int arr[][] = {{1,1,1} , {2,2,2} , {3,3,3}};
         // int arr2[][] ={{4,4,4} , {5,5,5} , {6,6,6}};
         // multiply2matrices(arr, arr2);
-
         
+        
+        // Transpose of a matrix
+        int arr[][] = {{1,1,1} , {2,2,2} , {3,3,3}};
+        // transposeofmatrix(arr,2,3);
+        transposeInPlace(arr, 3,3);
     }
 }
