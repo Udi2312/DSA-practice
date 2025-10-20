09734598e818fa44ec8a35690332b0c0b73f671d
Udit Bansal <udit.ban2312@gmail.com>
2025-10-20 19:53:09 +0530

    commit

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,5 +1,55 @@
 import java.util.Scanner;
 public class Recursion {
+    static int alternativesum(int n){
+        if(n==1){
+            return 1;
+        }
+        if(n%2==0){
+            return alternativesum(n-1) - n;
+        }
+        else{
+            return alternativesum(n-1) + n;
+        }
+    }
+    static void printmultiples(int nums, int k){
+        if(k==1){
+            System.out.print(nums + " ");
+            return;
+        }
+        printmultiples(nums, k-1);
+        System.out.print(nums * k + " ");
+    }
+    static int power2(int p, int q){
+        if(q==0){
+            return 1;
+        }
+        int smallpow = power2(p, q/2);
+        if(q%2==0){
+            return smallpow * smallpow;
+        }
+        else{
+            return p*smallpow*smallpow;
+        }
+    }
+    static int power(int p , int q){
+        int ans = 1;
+        if(q==1){
+            ans *= p;
+            return ans;
+        }
+        ans = power(p, q-1)*p;
+        return ans;
+    }
+    static int sumofdigits(int n){
+        int count = 0;
+        if(n>=0 && n<=9){
+            count += n;
+            return count;
+        }
+        count = sumofdigits(n/10) + n%10;
+        return count;
+    }
+
     static int fibonaccinumber(int n){
         if(n==0){
             return 0;
@@ -60,5 +110,41 @@ public class Recursion {
         // for(int i = 1; i<=n; i++){
         //     System.out.println(fibonaccinumber(i));
         // }
+
+
+        // Find the sum of digits of a number using recursion
+    //     System.out.println("Enter the value of n: ");
+    //     int n = s.nextInt();
+    //    System.out.println(sumofdigits(n));
+
+
+       // Find p^q using recursion
+    //    System.out.println("Enter p:");
+    //    int p = s.nextInt();
+    //    System.out.println("Enter q");
+    //    int q = s.nextInt();
+    //    System.out.println(power(p,q));
+
+
+    // Find p^q using recursion method 2
+    // System.out.println("Enter p:");
+    //    int p = s.nextInt();
+    //    System.out.println("Enter q");
+    //    int q = s.nextInt();
+    //    System.out.println(power2(p,q));
+
+
+    // Given nums and k, print first k multiples of nums
+    // System.out.println("Enter nums:");
+    //    int nums = s.nextInt();
+    //    System.out.println("Enter k");
+    //    int k = s.nextInt();
+    //   printmultiples(nums,k);
+
+
+    // Given a number n, find the sum of natural numbers till n but with alternative signs
+    System.out.println("Enter n: ");
+    int n = s.nextInt();
+    System.out.println(alternativesum(n));
     }
 }
