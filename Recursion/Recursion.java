b543ea05a9a81fc33b214c7b441dde8545c0e694
Udit Bansal <udit.ban2312@gmail.com>
2025-10-24 15:40:32 +0530

    commit

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,5 +1,56 @@
 import java.util.Scanner;
 public class Recursion {
+    static void returnallindices(int arr[] , int x, int i){
+        if(i == arr.length){
+            return;
+        }
+        if(arr[i] == x){
+            System.out.print(i + " ");
+        }
+         returnallindices(arr, x, i+1);
+    }
+    static int returnindexofelement(int arr[] , int x, int i){
+        if(i == arr.length){
+            return -1;
+        }
+        if(arr[i] == x){
+            return i;
+        }
+        return returnindexofelement(arr, x, i+1);
+    }
+    static boolean checknumexists(int arr[] , int x, int i){
+        boolean flag = false;
+        if(i == arr.length){
+            return flag;
+        }
+        flag = checknumexists(arr, x, i+1);
+        if(arr[i] == x){
+            return true;
+        }
+        return flag;
+    }
+    static int printsum(int arr[] , int i){
+        if(i == arr.length){
+            return 0;
+        }
+        int smallans = printsum(arr,i+1);
+        return smallans + arr[i];
+    }
+    static int printmaxvalue(int arr[] , int i){
+        if(i == arr.length-1){
+            return arr[i];
+        }
+       int smallans = printmaxvalue(arr, i+1);
+        return Math.max(arr[i] , smallans);
+    }
+    static void printarr(int arr[], int i){
+        if(i == arr.length-1){
+            System.out.print(arr[i]+ " ");
+            return;
+        }
+        System.out.print(arr[i] + " ");
+        printarr(arr, i+1);
+    }
     static int euclidalgo(int x , int y){
         if(y == 0){
             return x;
@@ -171,10 +222,37 @@ public class Recursion {
 
 
     // Find GCD using euclid's algorithm
-    System.out.println("Enter x: ");
-    int x = s.nextInt();
-    System.out.println("Enter y: ");
-    int y = s.nextInt();
-    System.out.println(euclidalgo(x,y));
+    // System.out.println("Enter x: ");
+    // int x = s.nextInt();
+    // System.out.println("Enter y: ");
+    // int y = s.nextInt();
+    // System.out.println(euclidalgo(x,y));
+
+
+    // Given an array, print all it's values recursively
+    // int arr[] = {1,2,4,5,6};
+    // printarr(arr, 0);
+
+
+    // Print the max value of the array
+    // int arr[] = {3,10,200,5,6,1,9};
+//    System.out.println( printmaxvalue(arr,0));
+
+
+
+// Print sum of elements of an array
+    // int arr[] = {3,10,20,5,6,1,9};
+    // System.out.println(printsum(arr,0));
+
+
+    // Check if the giiven number is present in the array or not
+    // int arr[] = {3,10,20,5,6,1,9};
+    // System.out.println(checknumexists(arr,9,0));
+
+
+    // If an element is present in an array, return the first index it occurs, else return -1
+    int arr[] = {2,10,20,2,6,2,9};
+    // System.out.println(returnindexofelement(arr,2,0));
+    returnallindices(arr,2,0);
     }
 }
