0f85a5a326f0a1a9a378b325c5fd1fc8019f1068
Udit Bansal <udit.ban2312@gmail.com>
2025-11-02 23:58:07 +0530

    commit

--- Binary Search/Binary_Search.java
+++ Binary Search/Binary_Search.java
@@ -1,4 +1,47 @@
 public class Binary_Search {
+    static int targetindex(int arr[] , int target){
+        int var = 0;
+        if(target < arr[arr.length-1]){
+            var = -1;
+        }
+        if(target > arr[arr.length-1]){
+            var = 1;
+        }
+
+        int st = 0, end = arr.length-1;
+        while(st <= end){
+            int mid = st + (end -st)/2;
+            if(arr[st] == target) return st;
+            if(arr[end] == target) return end;
+            if(arr[mid] == target){
+                return mid;
+            }
+            if(var == -1){
+                st = mid + 1;
+            }
+            if(var == 1){
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    static int leastelementinrotatedarray(int arr[]){
+        int st = 0, end = arr.length-1;
+        int ans = -1;
+        while( st <= end){
+            int mid = st + (end-st)/2;
+            if(arr[mid] > arr[arr.length-1]){
+                st = mid + 1;
+            }
+            else{
+                ans = mid;
+                end = mid -1;
+            }
+        }
+        return ans;
+    }
+
     static double squarerootwithprecision(int x, int precision){
         if(x< 0){
             System.out.println("Invalid Input.");
@@ -83,6 +126,16 @@ public class Binary_Search {
 
 
         // Calculate square root with precision
-        System.out.println(squarerootwithprecision(11,5));
+        // System.out.println(squarerootwithprecision(11,5));
+
+
+        // Find the index of minimum element in rotated sorted array
+        // int arr[] = {5,6,7,8,9,10,11,1,2,3,4};
+        // System.out.println(leastelementinrotatedarray(arr));
+
+
+        // return the index of target element in the rotated sorted array
+        int arr[] = {5,6,7,8,9,10,11,1,2,3,4};
+        System.out.println(targetindex(arr , 12));
     }
 }
