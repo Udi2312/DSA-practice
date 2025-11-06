a3630437ee20ba9f13efaef39aea4a86badcb63d
Udit Bansal <udit.ban2312@gmail.com>
2025-11-07 00:06:49 +0530

    commit

--- Binary Search/Binary_Search.java
+++ Binary Search/Binary_Search.java
@@ -1,4 +1,71 @@
 public class Binary_Search {
+    static boolean search2Dmatrix2(int arr[][] , int target){
+        int r = arr.length;
+        int c = arr[1].length;
+        int i = 0, j = c-1;
+        while(i<r && j>=0){
+            if(arr[i][j] == target){
+                return true;
+            }
+            else if(arr[i][j] > target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+    static boolean search2Dmatrix(int arr[][] , int target){
+        int r = arr.length;
+        int c = arr[1].length;
+        int st = 0, end = r*c-1;
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            int mr = mid/c;
+            int mc = mid%c;
+            if(arr[mr][mc] == target){
+                return true;
+            }
+            else if(arr[mr][mc] < target){
+                st = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+    static int targetindexrepeatedelements(int arr[] , int target){
+        int st = 0, end = arr.length-1;
+        while(st <= end){
+            int mid = st + (end -st)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[st] == arr[end] && arr[mid] == arr[end]){
+                st++;
+                end--;
+            }
+            else if(arr[mid] <= arr[end]){
+                if(target > arr[mid] && target <= arr[end]){
+                    st = mid + 1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+            else{
+                if(target >= arr[st] && target < arr[mid]){
+                    end = mid-1;
+                }
+                else{
+                    st = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
     static int targetindex(int arr[] , int target){
         int var = 0;
         if(target < arr[arr.length-1]){
@@ -135,7 +202,29 @@ public class Binary_Search {
 
 
         // return the index of target element in the rotated sorted array
-        int arr[] = {5,6,7,8,9,10,11,1,2,3,4};
-        System.out.println(targetindex(arr , 12));
+        // int arr[] = {5,6,7,8,9,10,11,1,2,3,4};
+        // System.out.println(targetindex(arr , 12));
+
+
+        // return the index of target element in the rotated sorted array where elements can be repeated
+        // int arr[] = {1,1,1,2,3,3,1,1,1};
+        // System.out.println(targetindexrepeatedelements(arr,2));
+
+
+        // You are given an m x n integer matrix matrix with the following two properties:
+        // Each row is sorted in non-decreasing order.
+        // The first integer of each row is greater than the last integer of the previous row.
+        // Given an integer target, return true if target is in matrix or false otherwise.
+
+        // int arr[][] = {{1,3,5,7} , {10,11,16,20} , {23,30,34,60}};
+        // System.out.println(search2Dmatrix(arr,60));
+
+
+        //Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+        // Integers in each row are sorted in ascending from left to right.
+        // Integers in each column are sorted in ascending from top to bottom.
+
+        // int arr[][] = {{1,4,7,11,15} , {2,5,8,12,20} , {3,6,9,16,22}};
+        // System.out.println(search2Dmatrix2(arr,19));
     }
 }
