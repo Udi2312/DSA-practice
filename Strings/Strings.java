901a289a55e7bba0d10e7e8109ed2f36410fb49d
Udit Bansal <udit.ban2312@gmail.com>
2025-12-13 01:01:33 +0530

    commit

---
+++ Strings/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        // Print all substrings of a given string
+        // String s = "abcd";
+        // for(int i = 0; i<s.length(); i++){
+        //     for(int j = i+1; j<s.length()+1; j++){
+        //         System.out.print(s.substring(i,j) + " ");
+        //     }
+        // }
+        
+    }
+}
