9bd865c770605b73d4ff58b10c6663a4d1e43f82
Udit Bansal <udit.ban2312@gmail.com>
2026-01-05 20:20:49 +0530

    commit

--- Linked List/LL1.java
+++ Linked List/LL1.java
@@ -1,4 +1,58 @@
 public class LL1{
+    public static class Linkedlist{
+        Node head = null;
+        Node tail = null;
+        void inserAtEnd(int val){
+            Node temp = new Node(val);
+            if(head==null){
+                head = temp;
+            }
+            else{
+                tail.next = temp;
+            }
+            tail = temp;
+        }
+        void display(){
+            Node temp = head;
+            while(temp != null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+        int size(){
+            Node temp = head;
+            int ans = 0;
+            while(temp != null){
+                ans++;
+                temp = temp.next;
+            }
+            return ans;
+        }
+        void insertAtStart(int val){
+            Node temp = new Node(val);
+            if(head == null){
+                // head = temp;
+                // tail = temp;
+                inserAtEnd(val);
+            }
+            else{
+                temp.next = head;
+                head = temp;
+
+            }
+        }
+        void insertAt(int idx , int val){
+            Node t = new Node(val);
+            Node temp = head;
+            for(int i = 1; i<= idx-1; i++){
+                temp = temp.next;
+            }
+            t.next = temp.next;
+            temp.next = t;
+
+        }
+    }
     public static int lengthofLL(Node head){
         int ans = 0;
         while(head != null){
@@ -22,6 +76,7 @@ public class LL1{
         System.out.print(head.data + " ");
         printLLUsingRecursion(head.next);
     }
+
     public static void printLL(Node head){
         Node temp = head;
         while(temp != null){
@@ -68,11 +123,29 @@ public class LL1{
         // printLLUsingRecursion(a);
 
 
+
         // Print LL in reverse order
         // printReverseLL(a);
 
 
+
         // Find Length of LL
-        System.out.println(lengthofLL(a));
+        // System.out.println(lengthofLL(a));
+
+
+
+        // Implementation of LinkedList
+        Linkedlist l = new Linkedlist();
+        l.inserAtEnd(4);
+        l.display();
+        l.inserAtEnd(5);
+        l.display();
+        // System.out.println(l.size());
+        l.insertAtStart(12);
+        l.display();
+        l.insertAtStart(13);
+        l.display();
+        l.insertAt(2, 15);
+        l.display();
     }
 }
