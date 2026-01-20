862c2495e3444318753406a5533b7b5c1823c21e
Udit Bansal <udit.ban2312@gmail.com>
2026-01-20 23:50:51 +0530

    commit

---
+++ Linked List/DLL.java
@@ -0,0 +1,54 @@
+public class DLL {
+    public static class Node{
+        int val;
+        Node next;
+        Node prev;
+        Node(int val){
+            this.val = val;
+        }
+    }
+    public static void printLL(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void printLLrev(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val + " ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+    public static void printLLrandom(Node random){
+        Node temp = random;
+        while(temp.prev != null){
+            temp = temp.prev;
+        }
+        printLL(temp);
+    }
+    public static void main(String[] args) {
+        Node a = new Node(4);
+        Node b = new Node(10);
+        Node c = new Node(2);
+        Node d = new Node(99);
+        Node e = new Node(13);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        b.prev = a;
+        c.prev = b;
+        d.prev = c;
+        e.prev = d;
+        // printLL(a);
+        // printLLrev(e);
+
+        // You are given any random node in a DLL, print the LL
+        printLLrandom(d);
+    }
+}
