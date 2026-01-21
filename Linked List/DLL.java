73e8b2c067cb95aeb3790639eee8b316a677ab60
Udit Bansal <udit.ban2312@gmail.com>
2026-01-21 23:22:23 +0530

    commit

--- Linked List/DLL.java
+++ Linked List/DLL.java
@@ -30,6 +30,37 @@ public class DLL {
         }
         printLL(temp);
     }
+    public static Node insertathead(Node head , int val){
+        Node f = new Node(val);
+        f.next = head;
+        head.prev = f;
+        head = f;
+        return head;
+    }
+    public static Node insertattail(Node head, int val){
+        Node f = new Node(val);
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = f;
+        f.prev = temp;
+        temp = head;
+        return temp;
+    }
+    public static Node insertatidx(Node head, int idx, int val){
+        Node t = new Node(val);
+        Node temp = head;
+        for(int i = 0; i<idx-1; i++){
+            temp = temp.next;
+        }
+        t.next = temp.next;
+        temp.next.prev = t;
+        temp.next = t;
+        t.prev = temp;
+        temp = head;
+        return temp;    
+    }
     public static void main(String[] args) {
         Node a = new Node(4);
         Node b = new Node(10);
@@ -49,6 +80,24 @@ public class DLL {
         // printLLrev(e);
 
         // You are given any random node in a DLL, print the LL
-        printLLrandom(d);
+        // printLLrandom(d);
+
+
+
+        // Insertion at head in DLL
+        // Node ans = insertathead(a,30);
+        // printLL(ans);
+
+
+        
+        // Insertion at tail in DLL, only head node is given
+        // Node ans = insertattail(a,22);
+        // printLL(ans);
+
+
+
+        // Insert at index in DLL
+        Node ans = insertatidx(a,3,55);
+        printLL(ans);
     }
 }
