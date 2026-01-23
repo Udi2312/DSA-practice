5f9ac9bc3941964a2fb03db8c779151b12d70395
Udit Bansal <udit.ban2312@gmail.com>
2026-01-23 16:59:20 +0530

    commit

--- Linked List/DLL.java
+++ Linked List/DLL.java
@@ -59,14 +59,50 @@ public class DLL {
         temp.next = t;
         t.prev = temp;
         temp = head;
-        return temp;    
+        return temp;
+    }
+    public static boolean checkpalendrome(Node head){
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        Node temp2 = head;
+        while(temp2 != null){
+            if(temp.val == temp2.val){
+                temp = temp.prev;
+                temp2 = temp2.next;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void targetsum(Node head, int target){
+        Node t = head;
+        while(t.next != null){
+            t = t.next;
+        }
+        Node h = head;
+        while(h.val < t.val){
+            if(h.val + t.val == target){
+                System.out.print(h.val +" "+ t.val);
+                break;
+            }
+           else if(h.val + t.val > target){
+                t = t.prev;
+            }
+            else{
+                h = h.next;
+            }
+        }
     }
     public static void main(String[] args) {
         Node a = new Node(4);
-        Node b = new Node(10);
-        Node c = new Node(2);
-        Node d = new Node(99);
-        Node e = new Node(13);
+        Node b = new Node(3);
+        Node c = new Node(9);
+        Node d = new Node(16);
+        Node e = new Node(20);
         a.next = b;
         b.next = c;
         c.next = d;
@@ -97,7 +133,21 @@ public class DLL {
 
 
         // Insert at index in DLL
-        Node ans = insertatidx(a,3,55);
-        printLL(ans);
+        // Node ans = insertatidx(a,3,55);
+        // printLL(ans);
+
+
+
+        // Check whether the given DLL is palendrome or not
+        // System.out.println(checkpalendrome(a));
+
+
+
+        // Given a sorted DLL and a target, print the numbers whose sum is equall to target
+        // targetsum(a,25);
+
+
+
+        // 
     }
 }
