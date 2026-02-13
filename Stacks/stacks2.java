73cb4a1196c0b305068f8e0ef22d197a8de6d300
Udit Bansal <udit.ban2312@gmail.com>
2026-02-13 12:21:22 +0530

    Commit

--- Stacks/stacks2.java
+++ Stacks/stacks2.java
@@ -38,6 +38,27 @@ public class stacks2 {
         }
         return ans;
     }
+    public static int[] nextGreater(int arr[]){
+        Stack<Integer> st = new Stack<>();
+        int ans[] = new int[arr.length];
+        st.push(arr[arr.length-1]);
+        ans[arr.length-1] = -1;
+        for(int i = arr.length-2; i>=0; i--){
+            if(st.peek() > arr[i]){
+                ans[i] = st.peek();
+            }
+            else if(st.peek() < arr[i]){
+                if(st.size() != 0){
+                    st.pop();
+                    
+                }
+                else{
+                    ans[i] = -1;
+                }
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         // Check whether the given string of brackets is valid or not
         // Scanner s = new Scanner(System.in);
@@ -46,9 +67,17 @@ public class stacks2 {
 
 
         // Remove the consecutive sub sequences inside the array whose length is greater than or equla to 2
-        int arr[] = {1,2,2,3,10,10,10,4,4,4,5,7,7,2};
-        int ans[] = removeSubSequences(arr);
-        for(int i = 0; i<ans.length; i++){
+        // int arr[] = {1,2,2,3,10,10,10,4,4,4,5,7,7,2};
+        // int ans[] = removeSubSequences(arr);
+        // for(int i = 0; i<ans.length; i++){
+        //     System.out.print(ans[i] + " ");
+        // }
+
+
+        // Find the next greater element for every element in the array and -1 if it does not exists
+        int arr[] = {1,3,2,1,8,6,3,4};
+        int ans[] = nextGreater(arr);
+        for(int i = 0; i<arr.length; i++){
             System.out.print(ans[i] + " ");
         }
     }
