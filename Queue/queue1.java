7d2a1b32961a7cd0d3f9d2549b3e176b3aeb8506
udit bansal <udit.ban2312@gmail.com>
2026-04-04 02:25:36 +0530

    commit

--- Queue/queue1.java
+++ Queue/queue1.java
@@ -1,24 +1,43 @@
-
 import java.util.LinkedList;
 import java.util.Queue;
 
-
 public class queue1{
     public static void main(String[] args) {
+        // Queue<Integer> q = new LinkedList<>();
+        // System.out.println(q.isEmpty());
+        // q.add(1);
+        // System.out.println(q.isEmpty());
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // q.add(5);
+        // System.out.println(q);
+        // q.remove();
+        // System.out.println(q);
+        // q.poll();
+        // System.out.println(q);
+        // System.out.println(q.element());
+        // System.out.println(q.size());
+
+
+        // Display queue
         Queue<Integer> q = new LinkedList<>();
-        System.out.println(q.isEmpty());
         q.add(1);
-        System.out.println(q.isEmpty());
         q.add(2);
         q.add(3);
         q.add(4);
         q.add(5);
+        Queue<Integer> help = new LinkedList<>();
+        while(q.size() > 0){
+            System.out.print(q.element() + " ");
+            help.add(q.element());
+            q.remove();
+        }
+        System.out.println();
+        while(help.size()>0){
+            q.add(help.element());
+            help.remove();
+        }
         System.out.println(q);
-        q.remove();
-        System.out.println(q);
-        q.poll();
-        System.out.println(q);
-        System.out.println(q.element());
-        System.out.println(q.size());
     }
 }
