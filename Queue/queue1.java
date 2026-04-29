772a9c1f7552bd535a8e389189f6ad05543aad64
udit bansal <udit.ban2312@gmail.com>
2026-04-29 22:51:20 +0530

    commit

--- Queue/queue1.java
+++ Queue/queue1.java
@@ -2,8 +2,9 @@
 public class queue1{
     public static class CircularQueue{
         int size = 0;
-        int f = r = -1;
-        int arr[] = new int[8];
+        int f = -1;
+        int r = -1;
+        int arr[] = new int[4];
         public void add(int x) throws Exception{
             if(size==0){
                 f = r = 0;
@@ -15,7 +16,55 @@ public class queue1{
             else if(r < arr.length-1){
                 arr[++r] = x;
             }
+            else if(r==arr.length-1){
+                r = 0;
+                arr[0] = x;
+            }
+            size++; 
+        }
+        
 
+        public int remove() throws Exception{
+            if(size == 0){
+                throw new Exception("Queue is empty");
+            }
+            else{
+                int a = arr[f];
+                if(f==arr.length-1) f = 0;
+                f++;
+                size--;
+                return a;
+            }
+        }
+
+        public int peek() throws Exception{
+            if(size==0){
+                throw new Exception("Queue is empty");
+            }
+            else{
+                return arr[f];
+            }
+        }
+
+        public void display(){
+            if(size==0){
+                System.out.println("Queue is empty");
+                return;
+            }
+            if(f<=r){
+                for(int i = f; i<=r; i++){
+                    System.out.print(arr[i] + " ");
+                }
+            }
+            else{
+                for(int i = f; i<arr.length; i++){
+                    System.out.print(arr[i] + " ");
+                }
+                for(int i = 0; i<r; i++){
+                    System.out.print(arr[i] + " ");
+                }
+            }
+            System.out.println();
         }
     }
     public static class Node{
@@ -132,7 +181,7 @@ public class queue1{
             System.out.println();
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // Queue<Integer> q = new LinkedList<>();
         // System.out.println(q.isEmpty());
         // q.add(1);
@@ -202,8 +251,16 @@ public class queue1{
 
 
         // Circular queue implementation
-
-
-        
+        CircularQueue q = new CircularQueue();
+        q.display();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.display();
+        q.remove();
+        q.display();
+        System.out.println(q.peek());
+        System.out.println(q.size);
     }
 }
